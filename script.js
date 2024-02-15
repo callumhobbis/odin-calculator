26e8a34f97fbf7a363e215b684a3900af8af6652
Callum Hobbis <callum.hobbis@gmail.com>
2024-02-15 15:34:58 +1300

    feat: add operate function

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+function operate(a, op, b) {
+  const ops = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+  };
+  return ops[op](a, b);
+}
